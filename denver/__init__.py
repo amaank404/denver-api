6db7f881744efd40d2255337b43ed50dd023f6b4
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-10-02 13:12:05 +0530

    Auto import all modules

--- denver/__init__.py
+++ denver/__init__.py
@@ -1,7 +1,24 @@
-'''
+"""
 Denver is a project targeting python developers for easy and fast development
 with the modules provided within it.
-'''
+"""
 
 __author__ = 'Xcodz'
 __version__ = '2020.6.4'
+
+
+from . import (bdtp,
+               bdtpfserv,
+               bitmap,
+               crypt,
+               ctext,
+               datau,
+               datp,
+               get,
+               graphics3d,
+               handypy,
+               log,
+               pysetup,
+               rawd,
+               sound,
+               text)
