eb014150bd12bd9a56673f0764672ccbad3945b6
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-10-23 23:19:34 +0530

    a small error fix

--- denver/tools/__main__.py
+++ denver/tools/__main__.py
@@ -1,5 +1,4 @@
 from denver import bcli
-from denver import ctext
 import os
 
 cli = bcli.new_cli()
@@ -13,9 +12,7 @@ def tool(path):
 
 if __name__ == '__main__':
     directory = os.path.dirname(__file__)
-    # noinspection PyCallByClass
-    print(ctext.ColoredText.escape("{green} Type"))
     cli.info("Available Tools")
     for x in os.listdir(directory):
-        if not x.startswith("_"):
+        if not x.startswith("_") and x.endswith(".py"):
             cli.info("\t", tool(x), sep='')
