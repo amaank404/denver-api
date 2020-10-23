b7170fafb86a3e3ca97f71c8ee691155d006e0a4
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-10-23 22:48:18 +0530

    Added a function to generate bindings upon device (enables a license to work for a specific device only)

--- denver/dlic.py
+++ denver/dlic.py
@@ -3,8 +3,9 @@ Denver type license
 """
 
 from getmac import get_mac_address
-from .crypt import crypt
+from . import crypt
 from hashlib import sha256
+from base64 import urlsafe_b64encode as encodebytes
 
 
 def generate_license(name, date_of_expiry, device_binding = None, encryption_key = None):
@@ -13,11 +14,11 @@ def generate_license(name, date_of_expiry, device_binding = None, encryption_key
 
 def generate_device_binding(device_information=None):
     if device_information is None:
-        device_information = get_mac_address()
-    return crypt.cipher.vigenere.encrypt(device_information, sha256(device_information.encode()).hexdigest())
+        device_information = get_mac_address().encode()
+    return crypt.encrypt(device_information, sha256(device_information).digest())
 
 
 def decrypt_device_binding(bindings, device_information=None):
     if device_information is None:
-        device_information = get_mac_address()
-    return crypt.cipher.vigenere.decrypt(bindings, sha256(device_information.encode()).hexdigest())
+        device_information = get_mac_address().encode()
+    return crypt.decrypt(bindings, sha256(device_information).digest())
