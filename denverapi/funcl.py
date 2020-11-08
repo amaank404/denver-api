2ea067b49854ef36957c88668c7df438203d6663
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-11-08 19:35:38 +0530

    More modifiers on debug argument

--- denverapi/funcl.py
+++ denverapi/funcl.py
@@ -15,20 +15,25 @@ class FunctionLogger:
         self.echo = True
         self.file = file
 
-    def debug(self, func):
-        @functools.wraps(func)
-        def function(*args, **kwargs):
-            if self.echo:
-                nargs = [repr(x) for x in args]
-                nkwargs = {k: repr(v) for k, v in kwargs.items()}
-                statement = func.__name__+"("
-                if len(nargs) != 0:
-                    statement += ", ".join(nargs)
-                if len(nargs) != 0 and len(nkwargs) != 0:
-                    statement += ", "
-                if len(nkwargs) != 0:
-                    statement += ", ".join([f"{k}={v}" for k, v in nkwargs.items()])
-                statement += ")"
-                print(f"Running {statement}", file=self.file)
-            func(*args, **kwargs)
-        return function
+    def debug(self, _func=None, *, action="Running"):
+        def decor(func):
+            @functools.wraps(func)
+            def function(*args, **kwargs):
+                if self.echo:
+                    nargs = [repr(x) for x in args]
+                    nkwargs = {k: repr(v) for k, v in kwargs.items()}
+                    statement = func.__name__+"("
+                    if len(nargs) != 0:
+                        statement += ", ".join(nargs)
+                    if len(nargs) != 0 and len(nkwargs) != 0:
+                        statement += ", "
+                    if len(nkwargs) != 0:
+                        statement += ", ".join([f"{k}={v}" for k, v in nkwargs.items()])
+                    statement += ")"
+                    print(f"{action}: {statement}", file=self.file)
+                func(*args, **kwargs)
+            return function
+        if _func is None:
+            return decor
+        else:
+            return decor(_func)
