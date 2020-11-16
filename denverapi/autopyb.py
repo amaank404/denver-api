8bb1c17a22f3197502e0c7f211270736c4a7d355
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-11-16 23:05:54 +0530

    Added more functionalities

--- denverapi/autopyb.py
+++ denverapi/autopyb.py
@@ -1,6 +1,10 @@
 import functools
 import argparse
 import sys
+import os
+import subprocess
+import pkgutil
+from . import install_pip_package
 try:
     from . import ctext
 except ImportError:
@@ -8,6 +12,54 @@ except ImportError:
 
 print = ctext.print
 input = ctext.input
+
+
+def run_command(command):
+    if isinstance(command, str):
+        return_code = os.system(command)
+    else:
+        return_code = subprocess.run(command)
+    return return_code
+
+
+def get_module_list():
+    return [x.name for x in pkgutil.iter_modules()]
+
+
+def ensure_pip_package(package, t="STABLE"):
+    if t.lower() == "stable":
+        install_pip_package(package)
+    elif t.lower() == "pre":
+        install_pip_package(package, pre=True)
+    elif t.lower() == "latest":
+        install_pip_package(package, update=True)
+    elif t.lower() == "pre-latest":
+        install_pip_package(package, pre=True, update=True)
+    else:
+        print(f"type '{t}' is not a valid option, skipping installation for '{package}'", fore="yellow")
+
+
+def make_platform_executable(name: str, script: str, t="ONEFILE", extras=None, hidden=None, *aflags):
+    if 'pyinstaller' not in get_module_list():
+        install_pip_package("pyinstaller")
+    if hidden is None:
+        hidden = []
+    if extras is None:
+        extras = []
+    t = [x.lower() for x in t.split('-')]
+    print(f"Making platform executable '{name}'")
+    flags: list = list(aflags)
+    flags.extend(["-n", name])
+    for x in extras:
+        flags.extend(["--add-data", x])
+    for x in hidden:
+        flags.extend(["--hidden-import", x])
+    if "onefile" in t:
+        flags.append("--onefile")
+    if "noconsole" in t:
+        flags.append("-w")
+    if run_command([sys.executable, "-m", "pyinstaller", script, *flags]) != 0:
+        raise EnvironmentError("PyInstaller Failed")
 
 
 # noinspection PyCallByClass
@@ -61,34 +113,3 @@ class BuildTasks:
             if args.command_ == x.__name__:
                 x(args.sub_arguments)
 
-
-auto = BuildTasks()
-
-
-@auto.task()
-def build_app():
-    print("Building App")
-
-
-@auto.task(build_app)
-def package_wheel():
-    print("Making Python Wheel")
-
-
-@auto.task(build_app)
-def package_source():
-    print("Packaging Source")
-
-
-@auto.task()
-def cleanup():
-    print("Cleaning Up")
-
-
-@auto.task(package_wheel, package_source, cleanup)
-def build_all():
-    print("Building All")
-
-
-if __name__ == "__main__":
-    auto.interact()
