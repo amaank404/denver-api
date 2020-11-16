e1e1006b069f366fa5371386977ff8859dfbcbdf
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-11-16 23:46:27 +0530

    bug fixes with autopyb

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
     packages=setuptools.find_packages()
     + setuptools.find_namespace_packages(include=["denverapi", "denverapi.*"]),
     package_data=find_package_data("denverapi", "denverapi"),
-    version="2.5.2",
+    version="2.5.3",
     author="xcodz",
     description="Denver API for python full-stack development",
     long_description=long_description,
