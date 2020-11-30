919673cde0255c393232571901b13f170ace85c0
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-11-30 19:14:55 +0530

    Update pyelib.py

--- denverapi/pyelib.py
+++ denverapi/pyelib.py
@@ -2,8 +2,9 @@
 Utilities to genrate a library out of python source
 """
 
-import types
 import os
+import types
+
 import dill
 
 
@@ -15,10 +16,10 @@ def make_pye(name: str, code: str):
     """
     Generate a PYE from source code
     """
-    
-    file = "lib"+os.path.basename(name)+".pye"
+
+    file = "lib" + os.path.basename(name) + ".pye"
     file_dir = os.path.dirname(name)
-    
+
     module = empty_module()
     exec(code, module.__dict__)
     dill.dump_session(os.path.join(file_dir, file), module)
@@ -32,8 +33,8 @@ def load_pye(name: str, environment=None):
     module = empty_module()
     if environment is not None:
         module.__dict__ = environment
-    
-    file = "lib"+os.path.basename(name)+".pye"
+
+    file = "lib" + os.path.basename(name) + ".pye"
     file_dir = os.path.dirname(name)
 
     dill.load_sessios(os.path.join(file_dir, file), module)
